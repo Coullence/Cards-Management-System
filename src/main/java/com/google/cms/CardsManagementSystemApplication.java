3b83cb8492073a35d6ae18c942f8959504c3296b
coullence <coullence@gmail.com>
2023-08-25 19:31:05 +0300

    Project Initialization

---
+++ src/main/java/com/google/cms/CardsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.google.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsManagementSystemApplication.class, args);
+	}
+
+}
